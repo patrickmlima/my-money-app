3d277c397ecf278e946c4d01f022800ddd47cf8a
Patrick Lima <patrickm.lima1@gmail.com>
2019-02-05 00:39:40 -0400

    Defined some error messages for database schema validation [backend]

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,4 +1,11 @@
 const mongoose = require('mongoose')
 
 mongoose.Promise = global.Promise
+
+// defining messages
+mongoose.Error.messages.general.required = "The attribute '{PATH}' is required."
+mongoose.Error.messages.Number.min = "Value '{VALUE}' is lesser than the minimal value required of '{MIN}'."
+mongoose.Error.messages.Number.max = "Value '{VALUE}' is greater than the max value required of '{MAX}'."
+mongoose.Error.messages.String.enum = "'{VALUE}' is not valid to attribute '{PATH}'."
+
 module.exports = mongoose.connect('mongodb://localhost/mymoney', { useNewUrlParser: true })
