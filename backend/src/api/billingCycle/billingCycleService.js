946baf885bc85af4259f9c681618ea69683db5b9
Patrick Lima <patrickm.lima1@gmail.com>
2019-02-06 00:17:44 -0400

    Added BillingCycle registers count rout [backend]

--- backend/src/api/billingCycle/billingCycleService.js
+++ backend/src/api/billingCycle/billingCycleService.js
@@ -3,4 +3,14 @@ const BillingCycle = require('./billingCycle')
 BillingCycle.methods(['get', 'post', 'put', 'delete'])
 BillingCycle.updateOptions({ new: true, runValidators: true })
 
+BillingCycle.route('count', ['get'], (req, res, next) => {
+  BillingCycle.countDocuments({}, (err, value) => {
+    if(err) {
+      res.status(500).json({ errors: [err] })
+    } else {
+      res.status(200).json({ value })
+    }
+  })
+})
+
 module.exports = BillingCycle
