7de90367ffa863e352c09535bbea7744c233d847
Patrick Lima <patrickm.lima1@gmail.com>
2019-02-06 00:39:30 -0400

    Added summary rout to BillingCycle [backend]

--- backend/src/api/billingCycle/billingCycleService.js
+++ backend/src/api/billingCycle/billingCycleService.js
@@ -13,4 +13,32 @@ BillingCycle.route('count', ['get'], (req, res, next) => {
   })
 })
 
+BillingCycle.route('summary', ['get'], (req, res, next) => {
+  BillingCycle.aggregate([
+    {
+      $project: {
+        credit: { $sum: "$credits.value" },
+        debt: { $sum: "$debts.value" }
+      }
+    },
+    {
+      $group: {
+        _id: null,
+        creadit: { $sum: "$credit" },
+        debt: { $sum: "$debt" }
+      }
+    },
+    {
+      $project: { _id: 0, credit: 1, debt: 1 }
+    }
+  ],
+  (err, result) => {
+    if(err) {
+      res.status(500).json({ errors: [err] })
+    } else {
+      res.status(200).json(result[0] || { credit: 0, debt: 0 })
+    }
+  })
+})
+
 module.exports = BillingCycle
