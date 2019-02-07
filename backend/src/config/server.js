a3b634b1df1fce0cfc0a8523dc476caaf7bfcad1
Patrick Lima <patrickm.lima1@gmail.com>
2019-02-07 00:58:19 -0400

    Added middleware to handler pagination [backend]

--- backend/src/config/server.js
+++ backend/src/config/server.js
@@ -3,12 +3,14 @@ const PORT = 3003
 const bodyParser = require('body-parser')
 const express = require('express')
 const allowCors = require('./cors');
+const queryParser = require('express-query-int');
 
 const server = express()
 
 server.use(bodyParser.urlencoded({ extended: true }))
 server.use(bodyParser.json())
 server.use(allowCors);
+server.use(queryParser());
 
 server.listen(PORT, function() {
   console.log(`Backend is running on port ${PORT}.`)
