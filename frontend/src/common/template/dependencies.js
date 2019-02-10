120b34708d2183f6c8a47a304ce956424e46b97c
Patrick Lima <patrickm.lima1@gmail.com>
2019-02-09 22:46:26 -0400

    Created project initial structure and files. Some template dependencies were imported [frontend]

---
+++ frontend/src/common/template/dependencies.js
@@ -0,0 +1,12 @@
+import 'modules/admin-lte/plugins/jQueryUI/jquery-ui.min'
+import 'modules/fastclick/lib/fastclick'
+import 'modules/slimscroll/lib/slimscroll'
+import 'modules/admin-lte/dist/js/adminlte.min'
+
+import 'modules/font-awesome/css/font-awesome.min.css'
+import 'modules/ionicons/dist/css/ionicons.min.css'
+import 'modules/admin-lte/dist/css/adminLTE.min.css'
+import 'modules/admin-lte/dist/css/skins/_all-skins.min.css'
+import 'modules/admin-lte/plugins/iCheck/flat/blue.css'
+
+import './custom.css'
