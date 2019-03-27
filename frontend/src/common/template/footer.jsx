4a9867d500b1c3f2ee47ab5853a4ac96b94756e2
Patrick Lima <patrickm.lima1@gmail.com>
2019-03-27 00:57:28 -0400

    Add footer component [frontend]

---
+++ frontend/src/common/template/footer.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export default props => (
+  <footer className='main-footer'>
+    <strong>Copyright &copy; 2019</strong>
+    <a href="http://cod3r.com.br" target="_blank">  Cod3r</a>
+  </footer>
+)
