ea3f9ccdf695e8c49889aa9c20ca889f452121eb
Patrick Lima <patrickm.lima1@gmail.com>
2019-10-06 16:53:55 -0400

    Integrate dashboard component with redux [frontend]

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,7 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { createStore, applyMiddleware } from 'redux'
+import { Provider } from 'react-redux'
+
+import promisse from 'redux-promise'
+
+import reducers from './main/reducers'
 import App from './main/app'
 
+const store = applyMiddleware(promisse)(createStore)(reducers)
 ReactDOM.render(
+  <Provider store={store}>
     <App />
+  </Provider>
   , document.getElementById("app"))
