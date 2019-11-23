95ebaa4b9d6e060ca88b0b0a6065a4ec87ec08aa
Patrick Lima <patrickm.lima1@gmail.com>
2019-11-23 14:48:42 -0400

    Create message component to show toastr messages on application [frontend]

--- frontend/src/billingCycle/billingCycleActions.js
+++ frontend/src/billingCycle/billingCycleActions.js
@@ -1,4 +1,5 @@
 import axios from 'axios'
+import { toastr } from 'react-redux-toastr'
 
 const BASE_URL = "http://localhost:3003/api"
 
@@ -11,9 +12,11 @@ export function getList() {
 }
 
 export function create(values) {
-    const req = axios.post(`${BASE_URL}/billing-cycles`, values)
+    axios.post(`${BASE_URL}/billing-cycles`, values)
+    .then(() => toastr.success("Sucesso", "Ciclo de pagamento salvo"))
+    .catch(err => err.response.data.errors.forEach(e => toastr.error("Erro", e)))
+
     return {
-        type: 'BILLING_CYCLE_CREATED',
-        payload: req
+        type: 'BILLING_CYCLE_CREATED'
     }
 }
--- frontend/src/main/app.jsx
+++ frontend/src/main/app.jsx
@@ -7,6 +7,7 @@ import Header from '../common/template/header'
 import Sidebar from '../common/template/sidebar'
 import Footer from '../common/template/footer'
 import Routes from './routes'
+import Messages from '../common/msg/messages'
 
 export default props => (
   <Router>
@@ -17,6 +18,7 @@ export default props => (
         <Routes />
       </div>
       <Footer />
+      <Messages />
     </div>
   </Router>
 )
