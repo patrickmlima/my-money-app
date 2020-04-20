f4d758da6452670b9a685cff47daa1b259affeb8
Patrick Lima <patrickm.lima1@gmail.com>
2020-04-20 02:27:27 -0400

    Fix: grid cols application [frontend]

--- frontend/src/common/layout/grid.jsx
+++ frontend/src/common/layout/grid.jsx
@@ -8,13 +8,13 @@ class Grid extends Component {
       classes += `col-xs-${cols[0]} `
     }
     if(cols[1]) {
-      classes += `col-sm-${cols[1]} `
+      classes += ` col-sm-${cols[1]} `
     }
     if(cols[2]) {
-      classes += `col-md-${cols[2]} `
+      classes += ` col-md-${cols[2]} `
     }
     if(cols[3]) {
-      classes += `col-lg-${cols[3]} `
+      classes += ` col-lg-${cols[3]} `
     }
     return classes
   }
